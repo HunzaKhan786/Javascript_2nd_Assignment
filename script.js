523d56b29ab9a694a17948740ecffa069d517c93
Muhammad faraz khan <47053490+Muhammadfarazkhan123@users.noreply.github.com>
2024-08-07 19:11:35 +0500

    add files

---
+++ script.js
@@ -0,0 +1,39 @@
+var rules ="<b>Rules for naming JS Variables</b>";
+document.write(rules);
+var number,$doller,_underscore;
+number="numbers,";
+$doller="$doller";
+_underscore="_ .";
+document.write("<br><br><br>Variable names can only contain ,"+" "+ number,  $doller," and"+" "+ _underscore," For example: $my_1stVariable.");
+var _rule2="<br><br>Variables must begin with a letter , $ or _ . For example $name, _name or name .";
+document.write(_rule2);
+var $rule3="<br><br>Variable names are case sensitive .";
+document.write($rule3);
+var rule_4="<br><br>Variable names should not be JS keywords . ";
+document.write(rule_4);
+
+// Declare three variable in one statament:
+var date,month,year;
+
+// Declare five  legall variable names:
+var $dollerSign;
+var number_1;
+var user;
+var Rs_$;
+var var_rules;
+
+// Declare five illegal variables names:
+// var 1_rule;
+// var user#2;
+// var 3*4;
+// var Rs;
+// var var;
+
+
+
+
+
+
+
+
+
