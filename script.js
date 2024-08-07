e8531777e20a4405cb785872f31900fdd08aaad8
Muhammad faraz khan <47053490+Muhammadfarazkhan123@users.noreply.github.com>
2024-08-08 03:10:35 +0500

    changes

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
+// chapter:VARIABLE NAMES: LEGAL & ILLEGAL:
+
+// 3. Display this in your browser:
+
+// a) A heading stating “Rules for naming JS variables":
+
 var rules ="<b>Rules for naming JS Variables</b>";
 document.write(rules);
+
+// b) Variable names can only contain ______, ______,
+// ______ and ______:For example $my_1stVariabl:
+
 var number,$doller,_underscore;
 number="numbers,";
 $doller="$doller";
 _underscore="_ .";
 document.write("<br><br><br>Variable names can only contain ,"+" "+ number,  $doller," and"+" "+ _underscore," For example: $my_1stVariable.");
+
+// c) Variables must begin with a ______, ______ or
+// _____. For example $name, _name or name
+
 var _rule2="<br><br>Variables must begin with a letter , $ or _ . For example $name, _name or name .";
 document.write(_rule2);
+
+// d) Variable names are case _________:
+
 var $rule3="<br><br>Variable names are case sensitive .";
 document.write($rule3);
+
+// e) Variable names should not be JS _________
+
 var rule_4="<br><br>Variable names should not be JS keywords . ";
 document.write(rule_4);
 
